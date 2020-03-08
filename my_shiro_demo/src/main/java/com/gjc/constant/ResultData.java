b9839a1f7ca882df038a30394550ee8ab2469578
Gjc <15168264328@163.com>
2020-03-08 15:54:12 +0800

    增加shiro项目

---
+++ my_shiro_demo/src/main/java/com/gjc/constant/ResultData.java
@@ -0,0 +1,61 @@
+package com.gjc.constant;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResultData implements java.io.Serializable{
+
+    /**
+     * 默认生成的序列号
+     */
+    private static final long serialVersionUID = 1L;
+
+    private int code = RetCode.FAIL.getCode();
+
+    private String msg = "";
+
+    private Map<String, Object> data = new HashMap<>(32);
+
+    public ResultData() {}
+
+    public ResultData(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public ResultData(int code, String msg, Map<String, Object> data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+
+    public Map<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, Object> data) {
+        this.data = data;
+    }
+
+    public void addData(String key, Object value) {
+        this.data.put(key, value);
+    }
+
+}
